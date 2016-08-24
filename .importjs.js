fcf87f0949d80fee701d82b7753b2e6f7d386a8d
Joe Lencioni <joe.lencioni@gmail.com>
2016-08-23 21:18:16 -0700

    Update import-js configuration for JS config
    The current version of import-js deprecated the JSON config for a JS
    config file.

---
+++ .importjs.js
@@ -0,0 +1,20 @@
+module.exports = {
+  aliases: {
+    $: "jquery",
+    React: "react",
+  },
+
+  namedExports: {
+    react: [
+      'Something',
+      'Foo',
+      'Bar',
+      'Fizz',
+      'Buzz',
+      'Baz',
+      'Oakland',
+      'Berkeley',
+      'PropTypes',
+    ],
+  },
+};
